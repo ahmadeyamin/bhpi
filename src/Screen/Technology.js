232ef707deb23a6bcaeb1a1ef904b04e32ebffe7
Ahmad Eyamin <ahmadeyamin@gmail.com>
2022-07-27 15:53:11 +0600

    New screen

--- src/Screen/Technology.js
+++ src/Screen/Technology.js
@@ -1,5 +1,5 @@
-import { Image, ImageBackground, SafeAreaView, ScrollView, StyleSheet, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native'
-import { useEffect, useLayoutEffect, useState } from 'react'
+import { Alert, Image, ImageBackground, Linking, SafeAreaView, ScrollView, StyleSheet, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native'
+import { useCallback, useEffect, useLayoutEffect, useState } from 'react'
 import * as Progress from 'react-native-progress';
 import { s } from '../Lib/Helper';
 import { useHeaderHeight } from '@react-navigation/elements';
@@ -38,6 +38,19 @@ const Technology = ({ route, navigation }) => {
     useLayoutEffect(() => {
         
     }, [navigation]);
+
+    const handlePress = async (url) => {
+        // Checking if the link is supported for links with custom URL scheme.
+        const supported = await Linking.canOpenURL(url);
+    
+        if (supported) {
+          // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+          // by some browser in the mobile
+          await Linking.openURL(url);
+        } else {
+          Alert.alert(`Don't know how to open this URL: ${url}`);
+        }
+    };
 
 
     if (isLoading) {
@@ -88,7 +101,7 @@ const Technology = ({ route, navigation }) => {
                 }}>
                     <Text style={styles.header_title}>{route.params.technology.name}</Text>
                     <Text style={styles.header_students}>{route.params.technology.description} Students</Text>
-                    <TouchableOpacity onPress={() => { }} style={styles.header_contact}>
+                    <TouchableOpacity onPress={()=>handlePress(`tel:${route.params.technology.phone}`)} style={styles.header_contact}>
 
                         <Icon 
                             name="phone" 
@@ -100,7 +113,7 @@ const Technology = ({ route, navigation }) => {
                         </Text>
 
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => { }} style={styles.header_contact}>
+                    <TouchableOpacity onPress={() => handlePress(`mailto:${route.params.technology.email}`)} style={styles.header_contact}>
 
                         <Icon 
                             name="email" 
@@ -121,7 +134,7 @@ const Technology = ({ route, navigation }) => {
                     <View style={styles.teacher_list}>
                         {teachers.map((teacher, index) => {
                             return (
-                                <TouchableOpacity key={index} onPress={() => {
+                                <View key={index} onPress={() => {
                                     
                                 }}>
                                     <View style={styles.teacher_item}>
@@ -133,14 +146,47 @@ const Technology = ({ route, navigation }) => {
                                             resizeMode="cover"
                                         />
                                         <View style={styles.teacher_info}>
-                                            <Text style={styles.teacher_name}>{teacher.name}</Text>
-                                            <Text style={styles.teacher_designation}>{teacher.title}</Text>
-                                            <Text style={styles.teacher_phone}>{teacher.phone}</Text>
-                                            <Text style={styles.teacher_email}>{teacher.email}</Text>
+                                            <View>
+                                                <Text style={styles.teacher_type}>
+                                                    {teacher.title}
+                                                </Text>
+                                                <Text numberOfLines={1} style={styles.teacher_name}>
+                                                    {teacher.name}
+                                                </Text>
+                                                <Text style={styles.teacher_sub}>
+                                                    {teacher.education}
+                                                </Text>
+                                            </View>
+                                            <View>
+                                                <TouchableOpacity onPress={() => handlePress(`tel:${teacher.phone}`)} style={styles.teacher_phone}>
+
+                                                    <Icon 
+                                                        name="phone" 
+                                                        size={20}  
+                                                        color='#444' 
+                                                    />
+                                                    <Text style={styles.header_text}>
+                                                        {teacher.phone ?? 'N\\A'}
+                                                    </Text>
+
+                                                </TouchableOpacity>
+                                                <TouchableOpacity onPress={() => handlePress(`mailto:${teacher.email}`)} style={styles.teacher_phone}>
+
+                                                    <Icon 
+                                                        name="email" 
+                                                        size={20}  
+                                                        color='#444' 
+                                                    />
+                                                    <Text style={styles.header_text}>
+                                                        {teacher.email ?? 'N\\A'}
+                                                    </Text>
+
+                                                </TouchableOpacity>
+                                            </View>
                                         </View>
                                         
                                     </View>
-                                </TouchableOpacity>
+                                </View>
                             )
                         }
                         )}
@@ -192,8 +238,56 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
 
     },
+    teacher_title:{
+        fontSize: s(20),
+        color: '#444',
+        fontWeight: 'bold',
+        marginBottom: s(10),
+        // textAlign: 'center',
+    },
+    
+    teacher_name:{
+        fontSize: s(15),
+        width: s(190),
+    },
+    teacher_sub:{
+        fontSize: s(12),
+        color: '#444',
+        marginBottom: s(5),
+    },
+    teacher_phone:{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+
+    },
+    teacher_type:{
+        fontSize: s(9),
+        color: '#555',
+    },  
+    teacher_container: {
+        padding: s(15),
+    },
     teacher_image: {
         width: s(100),
         height: s(100),
+        borderRadius: s(50),
+        marginRight: s(10),
+        borderColor: '#ddd',
+        borderWidth: 2,
+    },
+    teacher_item:{
+        flexDirection: 'row',
+        padding: s(10),
+        backgroundColor: '#fff',
+        marginVertical: s(5),
+        elevation: 4,
+        shadowOpacity: 0.3,
+        borderRadius: s(15),
+        overflow: 'hidden',
+    },
+    teacher_info:{
+        flexDirection: 'column',
+        justifyContent: 'space-between',
     }
 })
